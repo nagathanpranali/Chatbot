6aef8a41318c0182fcd2de1e52344fc2afc9e3a5
nagathanpranali <nagathanpranali@gmail.com>
2026-02-24 23:00:50 +0530

    chatbot app launch

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import *
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+data = pd.read_csv("/Users/pranalinagathan/Documents/1.kcNlp/Lec10/chatbot/chatbot.csv")
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+	chat = ""
+	if request.method == "POST":
+		old_chat = request.form["chat"]
+		qts = request.form["qts"]
+		qts = qts.strip().lower()
+		texts = [qts] + data["question"].str.lower().tolist()
+		cv = CountVectorizer()
+		vector = cv.fit_transform(texts)
+		cs = cosine_similarity(vector)
+		score = cs[0][1:]
+		data["score"] = score * 100
+		result = data.sort_values(by="score", ascending=False)
+		result = result[result.score > 10]
+		if len(result) == 0:
+			msg = "Persona: I’m not sure about that yet. You can ask me about my profile, experience, skills, or interests."
+		else:
+			ans = result.head(1)["answer"].values[0]
+			msg = "Persona: " + (ans)
+		new_chat = "You: " + qts + "\n" + msg
+		chat = old_chat + "\n" + new_chat
+		return render_template("home.html", msg=msg, chat=chat.strip())
+	else:
+		return render_template("home.html")
+
+#app.run(debug=True, use_reloader=True)
